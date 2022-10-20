0b8bf2d52853aa10ebe2d142e784434ef0303017
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2022-10-21 11:24:53 +1300

    Login JS done

--- javascript.js
+++ javascript.js
@@ -6,13 +6,36 @@ x.style.display = "none";
 function openLogin() {
   var x = document.getElementById("logIn");
   var y = document.getElementById("signUp");
+  var z = document.getElementById("openLoginBtn");
+  var a = document.getElementById("openSigninBtn");
+
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
   
+  if (z.style.display === "none") {
+    z.style.display = "block";
+    a.style.display = "block";
+  } else {
+    z.style.display = "none";
+    a.style.display = "none";
+  }
+
   y.style.display = "none";
+}
+
+function cancelLogin() {
+  var x = document.getElementById("logIn");
+  var y = document.getElementById("signUp");
+  var z = document.getElementById("openLoginBtn");
+  var a = document.getElementById("openSigninBtn");
+
+  x.style.display = "none";
+  y.style.display = "none";
+  z.style.display = "inline";
+  a.style.display = "inline";
 }
 
 //Set sign up window to hidden by default
@@ -22,24 +45,24 @@ x.style.display = "none";
 
 //Open sign up window, close log in window if it is already open
 function openSignup() {
-  var x = document.getElementById("signUp");
-  var y = document.getElementById("logIn");
-  
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-	y.style.display = "none";
-}
+  var x = document.getElementById("logIn");
+  var y = document.getElementById("signUp");
+  var z = document.getElementById("openLoginBtn");
+  var a = document.getElementById("openSigninBtn");
 
-function submitFunc() {
-	var x = document.getElementById("inputEmail");
-	if (var x === "") {
-		alert("Please enter an email");
-		return false;
-	}
-	else {
-		return true;
-	}
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+  
+  if (z.style.display === "none") {
+    z.style.display = "block";
+    a.style.display = "block";
+  } else {
+    z.style.display = "none";
+    a.style.display = "none";
+  }
+
+  x.style.display = "none";
 }
